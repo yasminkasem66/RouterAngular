3ca16bec66669feb4562bd540283fb859ef69e2a
yasminkassem66 <yasmin.abdellh@gmail.com>
2022-12-29 17:26:25 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './modules/test/home/home.component';
+import { ProductDetailsComponent } from './modules/test/product-details/product-details.component';
+import { ProductsComponent } from './modules/test/products/products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'products', component: ProductsComponent, outlet: 'prdouctRoute' },
+  { path: 'product/:id', component: ProductDetailsComponent, outlet: 'prdouctRoute' },
+  { path: '**', redirectTo: '' },
+
+  // {
+  //   path: '',
+  //   loadChildren: () => import('./modules/products/products.module').then(m => m.ProductsModule)
+  // },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './common/header/header.component';
+import { FooterComponent } from './common/footer/footer.component';
+import { SidebarComponent } from './common/sidebar/sidebar.component';
+import { MaterialModule } from './shared/material.module';
+import { ProductsComponent } from './modules/test/products/products.component';
+import { ProductDetailsComponent } from './modules/test/product-details/product-details.component';
+import { ProductsSidebarComponent } from './modules/test/products-sidebar/products-sidebar.component';
+import { HomeComponent } from './modules/test/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    ProductsComponent,
+    ProductDetailsComponent,
+    SidebarComponent,
+    ProductsSidebarComponent,
+    HomeComponent,
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
